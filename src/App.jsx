356363b8daf413824b37d76af6034ff14292b930
SakshiSharan1912 <sakssha19@gmail.com>
2025-03-23 12:11:56 +0530

    added contact page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,4 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-import SocialSidebar from './components/SocialSidebar';
 import { BrowserRouter as Router, Route,Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home/Home.jsx';
@@ -18,22 +12,9 @@ import Newsroom from './pages/Newsroom/Newsroom.jsx';
 import Contact from './pages/Contact/Contact.jsx';
 import './App.css';
 
-// Create a wrapper component to use the useLocation hook
-const AppContent = () => {
-  const location = useLocation();
-  const showSocialSidebar = location.pathname !== '/contact';
-
+function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <Navbar />
-      {showSocialSidebar && <SocialSidebar />}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/contact" element={<Contact />} />
-        {/* Add other routes as needed */}
-      </Routes>
-=======
       <Router>
         <Navbar />
       <Routes>
@@ -43,23 +24,15 @@ const AppContent = () => {
          <Route path='/tech' element={<Technology/>} />
          <Route path='/resource' element={<Resources/>} />
          <Route path='/newsroom' element={<Newsroom/>} />
+         <Route path='/contact' element={<Contact/>} />
       </Routes>
       
     
->>>>>>> 53509f6d3352d41d21d60387f8aed6a4add9d1f4
       <Footer />
       </Router>
       
     </div>
   );
-};
-
-function App() {
-  return (
-    <Router>
-      <AppContent />
-    </Router>
-  );
 }
 
-export default App; 
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,5 @@
 import React, { useState } from 'react';
-<<<<<<< HEAD
-import { Link } from 'react-router-dom';
-=======
 import { Link,useNavigate } from 'react-router-dom';
->>>>>>> 53509f6d3352d41d21d60387f8aed6a4add9d1f4
 import './Navbar.css';
 
 const Navbar = () => {
@@ -17,45 +13,30 @@ const Navbar = () => {
     <header>
       <nav>
         <div className="logo">
-          <Link to="/"><h1>Agrix</h1></Link>
+          <h1>Agrix</h1>
         </div>
         <ul className="nav-links">
-<<<<<<< HEAD
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-=======
           <li><Link to='/'>Home</Link> </li>
           <li><Link to='/about'>About</Link></li>
->>>>>>> 53509f6d3352d41d21d60387f8aed6a4add9d1f4
           <li className="solutions-dropdown">
-            <a href="#" onClick={(e) => {
+            <a href="#solutions" onClick={(e) => {
               e.preventDefault();
               setIsDropdownOpen(!isDropdownOpen);
             }}>Solutions</a>
             {isDropdownOpen && (
               <ul className="dropdown-menu">
-<<<<<<< HEAD
-                <li><Link to="/services" onClick={handleOptionClick}>Services</Link></li>
-=======
                 <li><Link to='/servicess' onClick={handleOptionClick}>Services</Link></li>
->>>>>>> 53509f6d3352d41d21d60387f8aed6a4add9d1f4
                 <li><Link to="/technology" onClick={handleOptionClick}>Technology</Link></li>
               </ul>
             )}
           </li>
-<<<<<<< HEAD
-          <li><Link to="/resources">Resources</Link></li>
-          <li><Link to="/newsroom">Newsroom</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
-=======
           <li><Link to='/resouces'>Resources</Link></li>
           <li><Link to='/newsroom'>Newsroom</Link></li>
           <li><Link to='/contact'>Contact</Link></li>
->>>>>>> 53509f6d3352d41d21d60387f8aed6a4add9d1f4
         </ul>
       </nav>
     </header>
   );
 };
 
-export default Navbar; 
+export default Navbar;
