4cdefed51c16d6ed660aad4efe40ef7557281729
snehasahu7 <btech15146.22@bitmesra.ac.in>
2025-06-07 12:16:35 +0530

    backend updated

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const app = express();
 const port = 5000;
 app.use(express.json());
 app.use(cors({
-    origin:'https://agrixagrotech.netlify.app/',
+    origin:'https://agrixagrotech.netlify.app/contact',
     methods:['POST']
 }));
 
